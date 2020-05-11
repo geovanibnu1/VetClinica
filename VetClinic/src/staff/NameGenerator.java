c33953c4c0c7735b7a2107f6188ddf3d6c2a1b0c
Geovani <geovanibnu1@gmail.com>
2020-05-11 13:38:17 +0100

    add list of names

--- VetClinic/src/staff/NameGenerator.java
+++ VetClinic/src/staff/NameGenerator.java
@@ -6,10 +6,16 @@ public class NameGenerator {
 
 	
 	// Array for names
-	private String[] firstName = {"Katerina", "Atticus", "Menachem", "Brooklyn", "Haaris", "Brittney", "Tiah", "Anya", "Melina", "Huzaifa", "Mehreen", "Paloma", "Carole", "Rabia", "Trevor", "Anisa", "Yvette", "Heather", "Jacqueline", "Amaya", "Zoey", "Benjamin"};
+	private String[] firstName = {"Katerina", "Atticus", "Menachem", "Brooklyn", "Haaris", "Brittney",
+			"Tiah", "Anya", "Melina", "Huzaifa", "Mehreen", "Paloma", "Carole", "Rabia", "Trevor", "Anisa",
+			"Yvette", "Heather", "Jacqueline", "Amaya", "Zoey", "Benjamin", "Cara", "Yareli", "Wendy ", "Addison", "Kymani",
+			"Kolton", "Bruno", "Belton"};
 	
 	// Array for surname
-	private String[] surName = {"Arellano", "Brewer", "Wallis", "Benton", "Cottrell", "Castro", "Spears", "Wolfe", "Mac", "Cole", "Drake", "Leonard", "Roth", "Mill", "Corbett", "Rooney", "Marriott", "Driscoll", "Hobbs", "Millar", "Jefferson", "O'Quinn"};
+	private String[] surName = {"Arellano", "Brewer", "Wallis", "Benton", "Cottrell", "Castro", "Spears",
+			"Wolfe", "Mac", "Cole", "Drake", "Leonard", "Roth", "Mill", "Corbett", "Rooney", "Marriott",
+			"Driscoll", "Hobbs", "Millar", "Jefferson", "O'Quinn", "Kramer", "Terrell", "Zhang", "King", "Jorra", "Fitsburg", "McDonnald",
+			"Dracker"};
 	
 	public NameGenerator() {
 		// TODO Auto-generated constructor stub
